54c24f5dd0e74e2d2409d2673817549b4653e525
athul <athulkharvi1234@gmail.com>
2024-11-07 00:21:55 +0530

    builded chagnes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default {
+  build: {
+    outDir: 'build', // Ensure it's 'build' or update Netlify's config to match 'dist'
+  },
+}
